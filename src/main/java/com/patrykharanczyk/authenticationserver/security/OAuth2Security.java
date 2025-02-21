407b970d74c5b79052e8e80697f66484390e0413
patryk.haranczyk <patryk023@gmail.com>
2025-02-21 21:41:58 +0100

    Added Client model and repo

--- src/main/java/com/patrykharanczyk/authenticationserver/security/OAuth2Security.java
+++ src/main/java/com/patrykharanczyk/authenticationserver/security/OAuth2Security.java
@@ -7,16 +7,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.UUID;
 
 @Configuration
 @EnableMethodSecurity
@@ -33,20 +26,6 @@ public class OAuth2Security {
                         .anyRequest().authenticated()
                 )
                 .build();
-    }
-
-    @Bean
-    public RegisteredClientRepository registeredClientRepository() {
-        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("resolutions-client")
-                .clientSecret("{noop}secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .scope("resolution:read")
-                .scope("resolution:write")
-                .build();
-
-        return new InMemoryRegisteredClientRepository(registeredClient);
     }
 
     @Bean
